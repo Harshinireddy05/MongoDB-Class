1b49dd6f15669712ba9be5e66d6672983c5f7e18
Harshinireddy05 <Harshinireddynellore@gmail.com>
2024-07-08 10:01:23 +0530

    CRUD Operations

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
+const app = express()
+
+app.use(express.json());
 
 mongoose
     .connect(
@@ -34,11 +36,63 @@ const ProductSchema = new mongoose.Schema({
         required:true
     }
 
+
+});
+
+
+const productModel = mongoose.model('products', ProductSchema);
+
+//create
+
+app.post('/api/products', async (req, res) => {
+    await productModel.create({
+        product_name: req.body.product_name,
+        product_price: req.body.product_price,
+        isInStock: req.body.isInStock,
+        category: req.body.category
+    })
+
+    return res.status(201).json({message:"Product Created"})
+})
+
+//get route
+
+app.get('/api/products', async (req, res) => {
+    
+    const allProducts = await productModel.find({isInStock:true})
+
+    return res.json(allProducts)
+
+})
+
+//get product by ID
+
+app.get('/api/products/:id', async (req, res) => {
+        
+    const product = await productModel.findById(req.params.id)
+    
+    return res.json(product)
     
 })
 
 
-const app = express()
+// Update product
+
+app.put('/api/products/:id' , async(req , res)=>{
+
+    const updatedProduct = await productModel.findByIdAndUpdate(req.params.id , req.body)
+
+    return res.json(updatedProduct)
+})
+
+//Delete product
+
+app.delete('/api/products/:id' , async(req , res)=>{
+    
+    const deletedProduct = await productModel.findByIdAndDelete(req.params.id)
+
+    return res.json(deletedProduct)
+})
 
 app.listen(8086, () => {
     console.log('Server started at port 8086')
